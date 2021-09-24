b21fefa09db03b147863beaa6207a436ab75c66c
unknown <fateh_djehinet@outlook.com>
2021-09-25 00:41:51 +0100

    fix deploy issue

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/xplore-wilderness-client"));
+app.use(express.static("./dist/XploreWildernessClient"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/xplore-wilderness-client/" })
+  res.sendFile("index.html", { root: "dist/XploreWildernessClient/" })
 );
 
 // Start the app by listening on the default Heroku port
