9b09b5669a5376a4c33a9fe465a2fec6cd76dab1
unknown <fateh_djehinet@outlook.com>
2021-09-26 21:14:26 +0100

    wip

--- src/app/guide/guide-insurance/guide-insurance.component.ts
+++ src/app/guide/guide-insurance/guide-insurance.component.ts
@@ -1,15 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-guide-insurance',
   templateUrl: './guide-insurance.component.html',
-  styleUrls: ['./guide-insurance.component.scss']
+  styleUrls: ['./guide-insurance.component.scss'],
 })
 export class GuideInsuranceComponent implements OnInit {
+  @ViewChild('fileLoader1', { static: true })
+  fileLoader1: ElementRef;
+  @ViewChild('fileLoader2', { static: true })
+  fileLoader2: ElementRef;
 
-  constructor() { }
+  fileToUpload: any;
+  doc1: string = '';
+  doc2: string = '';
+  constructor(private cd: ChangeDetectorRef, private snackbar: MatSnackBar) {}
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  ngOnInit(): void {
+  pickFile1() {
+    (<any>this.fileLoader1.nativeElement).click();
+    // this._elementRef.nativeElement.querySelector('input').click();
+  }
+  pickFile2() {
+    (<any>this.fileLoader2.nativeElement).click();
+    // this._elementRef.nativeElement.querySelector('input').click();
   }
 
+  deleteImage1() {
+    this.doc1 = '';
+    setTimeout(() => {
+      this.cd.markForCheck();
+    }, 300);
+  }
+  deleteImage2() {
+    this.doc2 = '';
+    setTimeout(() => {
+      this.cd.markForCheck();
+    }, 300);
+  }
+  ngOnInit(): void {
+    this.snackbar.open(
+      'All our guides are required to have their identity verified before being able to create and lead trips. Please upload two pieces of official identification for verification. Official identification can include Passport,National ID, and Health Cards.',
+      'close',
+      {
+        duration: 60000,
+        panelClass: ['success-snackbar'],
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      }
+    );
+  }
+  onSelectFile1(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      console.log(event.target.files);
+      this.fileToUpload = event.target.files[0];
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // tslint:disable-next-line:no-shadowed-variable
+      reader.onload = (event: any) => {
+        // called once readAsDataURL is completed
+        this.doc1 = event.target.result;
+        setTimeout(() => {
+          this.cd.markForCheck();
+        }, 300);
+      };
+    }
+  }
+  onSelectFile2(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      console.log(event.target.files);
+      this.fileToUpload = event.target.files[0];
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // tslint:disable-next-line:no-shadowed-variable
+      reader.onload = (event: any) => {
+        // called once readAsDataURL is completed
+        this.doc2 = event.target.result;
+        setTimeout(() => {
+          this.cd.markForCheck();
+        }, 300);
+      };
+    }
+  }
 }
